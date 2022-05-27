dba19eebc147634ccb650dbbefcbcb431b72e007
Leandro L. Vilela <leandrolvilela@gmail.com>
2022-05-27 19:45:33 -0300

    visualização das tabelas no admin

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,3 +1,4 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth.models import User
+u = User.objects.get(username='admin')
+u.set_password('new password')
+u.save()
