dfa98814a9270a9430f3789c8014296be42e728f
Leandro L. Vilela <leandrolvilela@gmail.com>
2022-06-01 20:41:16 -0300

    opção de buscar

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,4 +1,6 @@
+from unicodedata import name
 from django.urls import path
+from django.views import View
 
 from . import views
 
@@ -7,5 +9,6 @@ urlpatterns = [
     # o primeiro parametro da função path 'receita' aparece no endereço da URL (http://127.0.0.1:8000/receita)
     # o nome que está depois views. é a nome do método que está no views.py
     # o nome que está no name é utilizado no HTML {% url 'receita'%}
-    path('<int:receita_id>', views.receita, name='receita')   
+    path('<int:receita_id>', views.receita, name='receita'),
+    path('buscar', views.buscar, name='buscar')
 ]
