e4184563191804fc537f2589824a7081598b74ae
Leandro L. Vilela <leandrolvilela@gmail.com>
2022-06-04 21:37:30 -0300

    tela de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('cadastro')
+
+        if senha != senha2:
+            print('As semhas não são iguais')
+            return redirect('cadastro')
+
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado!')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome,email=email, password=senha)
+        user.save()
+        print(f'Usuário: {nome}, cadastrado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        print(email, senha)
+        
     return render(request, 'usuarios/login.html')
 
 def logout(request):
